5d399646ef8d06b965fff394adbd78204c72e7f1
Arun Mohan Sharma <arun-mark-iii@Aruns-MBP.bbrouter>
2022-09-29 12:16:42 +0530

    adding pages routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css'
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/signup">
+            <Signup/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/login/Login.js
@@ -0,0 +1,8 @@
+import styles from "./Login.module.css"
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
