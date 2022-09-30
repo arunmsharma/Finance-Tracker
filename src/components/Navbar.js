8b0ea66cbfe052c4546e819f71977242b73d9875
Arun Mohan Sharma <arun-mark-iii@Aruns-MBP.bbrouter>
2022-09-30 13:24:45 +0530

    creating a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import styles from './Navbar.module.css'
+
+
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+        <ul>
+            <li className={styles.title}>
+                myMoney 
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
