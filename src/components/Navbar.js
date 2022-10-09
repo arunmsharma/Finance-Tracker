c1903eb9c763e5cfa9ba2546bbdc7391958f70fc
Arun Mohan Sharma <arun-mark-iii@Aruns-MBP.bbrouter>
2022-10-09 10:54:04 +0530

    conditionaly showing user content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,41 @@
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
-
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
+  const { logout } = useLogout();
 
-  const {logout} = useLogout()
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
-        <ul>
-            <li className={styles.title}>
-                myMoney 
+      <ul>
+        <li className={styles.title}>myMoney</li>
+
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
             </li>
             <li>
-                <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
             </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
             <li>
-            <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <button className='btn' onClick={logout}>
+              <button className="btn" onClick={logout}>
                 Logout
               </button>
             </li>
-        </ul>
+          </>
+        )}
+      </ul>
     </nav>
-  )
+  );
 }
