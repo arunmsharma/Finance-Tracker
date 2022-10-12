e085e4835a221dd952dd953fe4bd8e234ccdb8d5
Arun Mohan Sharma <arun-mark-iii@Aruns-MBP.bbrouter>
2022-10-13 00:15:38 +0530

    firebase auth state changes so we prevent loading null user at first load of website

--- src/ context/AuthContext.js
+++ src/ context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { projectAuth } from "../firebase/config";
 
 export const AuthContext = createContext()
 
@@ -10,7 +11,10 @@ export const authReducer = (state,action) => {
             
         case 'LOGOUT':
             return {...state,user:null}
-        
+
+        case 'AUTH_IS_READY':
+            return {...state,user:action.payload,authIsReady:true}
+
         default:
             return state            
     }
@@ -19,11 +23,21 @@ export const authReducer = (state,action) => {
 export const AuthContextProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(authReducer, {
-        user: null
+        user: null,
+        authIsReady: false
     })
 
-    console.log('Authcontext state:', state);
+    useEffect(()=>{
+        const unsub = projectAuth.onAuthStateChanged(
+            (user)=>{
+                dispatch({type:'AUTH_IS_READY',payload:user})
+                unsub()
+            }
+        )
+        
+    },[])
 
+    console.log('Authcontext state:', state);
 
     return (
         <AuthContext.Provider value={{...state,dispatch}}>
