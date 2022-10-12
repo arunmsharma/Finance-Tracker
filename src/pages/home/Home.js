8308ae19573ca55b80acd5815832eb094a7eabc2
Arun Mohan Sharma <arun-mark-iii@Aruns-MBP.bbrouter>
2022-10-13 00:35:08 +0530

    making a transaction form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,16 @@
 import styles from "./Home.module.css"
 
-import React from 'react'
+import TransactionForm from "./TransactionForm"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        transaction list
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm/>
+      </div>
+    </div>
   )
 }
